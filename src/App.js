2fcd04851d0ef00b7b5c9115d6294d26450ce3b8
Youhei <kagkaggru@gmail.com>
2023-10-15 17:54:27 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
   return (
     <div>
       <Header>
-    　　
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HZ0PSM326S"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)};
-          gtag('js', new Date());
-
-          gtag('config', 'G-HZ0PSM326S');
-        </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-HZ0PSM326S"></script>
+      
    　 </Header>
       <MainContent />
       <Footer />
