6809f1a8674627a7e186c00b964e8569845a7eb2
Youhei <126801078+You8006@users.noreply.github.com>
2023-10-15 17:33:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import MainContent from './MainContent';
 function App() {
   return (
     <div>
-      <Header />
+      <head>
+    　　<!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HZ0PSM326S"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-HZ0PSM326S');
+        </script>
+   　 </head>
       <MainContent />
       <Footer />
     </div>
